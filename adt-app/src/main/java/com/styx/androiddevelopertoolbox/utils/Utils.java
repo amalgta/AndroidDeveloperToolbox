875784828a8cd8a4418ea1ff457f39bfeebe1cbc
Amal Thomas <amalgta@gmail.com>
2016-06-16 00:38:58 +0530

    Initial commit

---
+++ adt-app/src/main/java/com/styx/androiddevelopertoolbox/utils/Utils.java
@@ -0,0 +1,10 @@
+package com.styx.androiddevelopertoolbox.utils;
+
+/**
+ * Created by amalg on 16-06-2016.
+ */
+public class Utils {
+    public String getTitle(){
+        return "It Worked. Congratualtinons";
+    }
+}
